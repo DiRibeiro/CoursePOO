9451868cddb12a6ada4c05919a5b3db996bbcde3
'DiRibeiro' <'diribeiro740@gmail.com'>
2020-02-19 00:15:38 -0300

    Update code exercise 2

--- Exercises/src/Lista01.java
+++ Exercises/src/Lista01.java
@@ -13,8 +13,27 @@ public class Lista01 {
 		
 	}
 	
-	/*
-	 */
+	/*2. Sistema de banco*/
+	public static void bank() {
+		double P = 0; //Empréstimo
+		double A = 0; //Valor pago mensalmente
+		double i = 0; //Porcentagem de juros do saldo corrente
+		
+		//Valor referente aos juros pagos em dinheiro
+		
+		//Valor em dinheiro aplicado em pagamento da dívida
+		
+		//Valor acumulado de juros já pagos
+		
+		//Valor a ser pago ao fim de cada mês;
+		
+		//No final do programa
+		//Número de meses necessário para pagamento do empréstimo
+		
+		//Quantidade da última prestação
+		
+		
+	}
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		//1
